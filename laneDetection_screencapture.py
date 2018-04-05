4dd37975dce05b3a798217be5afd3b36f0676b25
ananaza <antti.ylikojola@gmail.com>
2018-04-05 22:46:46 +0300

    separate line detection and continuous lines

--- laneDetection_screencapture.py
+++ laneDetection_screencapture.py
@@ -1,23 +1,28 @@
 from time import sleep
 import cv2
 import numpy as np
-import win32gui, win32ui, win32con, win32api
+import math
+import win32gui
+import win32ui
+import win32con
+import win32api
 
 # https://www.programcreek.com/python/example/14102/win32gui.GetDesktopWindow
+
+
 def grab_screen(region=(0, 53, 960, 590)):
 
     hwin = win32gui.GetDesktopWindow()
 
     if region:
-            left,top,x2,y2 = region
-            width = x2 - left + 1
-            height = y2 - top + 1
+        left, top, x2, y2 = region
+        width = x2 - left + 1
+        height = y2 - top + 1
     else:
         width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
         height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
         left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
         top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
-
 
     hwindc = win32gui.GetWindowDC(hwin)
     srcdc = win32ui.CreateDCFromHandle(hwindc)
@@ -26,17 +31,17 @@ def grab_screen(region=(0, 53, 960, 590)):
     bmp.CreateCompatibleBitmap(srcdc, width, height)
     memdc.SelectObject(bmp)
     memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
-    
+
     signedIntsArray = bmp.GetBitmapBits(True)
     img = np.fromstring(signedIntsArray, dtype='uint8')
-    img.shape = (height,width,4)
+    img.shape = (height, width, 4)
 
     srcdc.DeleteDC()
     memdc.DeleteDC()
     win32gui.ReleaseDC(hwin, hwindc)
     win32gui.DeleteObject(bmp.GetHandle())
-    print(cv2.cvtColor(img, cv2.COLOR_BGRA2RGB).shape)
     return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+
 
 def process_img(original_image):
 
@@ -45,6 +50,11 @@ def process_img(original_image):
         height = original_image.shape[0]
         region_of_interest_vertices = [
             (0, height), (width / 2, height / 2), (width, height)]
+
+    left_line_x = []
+    left_line_y = []
+    right_line_x = []
+    right_line_y = []
 
     gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     canny_image = cv2.Canny(gray_image, 100, 200)
@@ -61,7 +71,43 @@ def process_img(original_image):
         maxLineGap=25
     )
 
-    return cropped_image, lines
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            slope = (y2 - y1) / (x2 - x1) 
+            if math.fabs(slope) < 0.5:  
+                continue
+            if slope <= 0: 
+                left_line_x.extend([x1, x2])
+                left_line_y.extend([y1, y2])
+            else: 
+                right_line_x.extend([x1, x2])
+                right_line_y.extend([y1, y2])
+ 
+    min_y = int(original_image.shape[0] * (3 / 5))
+    max_y = int(original_image.shape[0])
+
+    poly_left = np.poly1d(np.polyfit(
+        left_line_y,
+        left_line_x,
+        deg=1
+    ))
+    left_x_start = int(poly_left(max_y))
+    left_x_end = int(poly_left(min_y))
+        
+    poly_right = np.poly1d(np.polyfit(
+        right_line_y,
+        right_line_x,
+        deg=1
+    ))
+    right_x_start = int(poly_right(max_y))
+    right_x_end = int(poly_right(min_y))
+
+    processed_lines = [[
+        [left_x_start, max_y, left_x_end, min_y],
+        [right_x_start, max_y, right_x_end, min_y],
+    ]]
+
+    return cropped_image, processed_lines
 
 
 def region_of_interest(img, vertices):
@@ -87,8 +133,9 @@ def draw_lines(img, vertices):
     # luodaan kaistan piirrolle musta kuva
     line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
 
+
     # piirretaan kaistaviivat
-    for line in lines:
+    for line in processed_lines:
         for x1, y1, x2, y2 in line:
             cv2.line(line_img, (x1, y1), (x2, y2), [250, 250, 0], 5)
 
@@ -97,17 +144,17 @@ def draw_lines(img, vertices):
 
     return img
 
+
 while True:
 
-
     original_image = grab_screen()
-    cropped_image, lines = process_img(original_image)
+    cropped_image, processed_lines = process_img(original_image)
 
-    if lines is not None:
+    if processed_lines is not None:
                                                 # piirra viivat
-        image_with_lines = draw_lines(original_image, lines)
+        image_with_lines = draw_lines(original_image, processed_lines)
         cv2.imshow("frame", image_with_lines)
-
+    cv2.imshow("asas", cropped_image)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
